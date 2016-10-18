34b8c1bb4e6a338d6766b50468f7d2e9af3681e5
Aladeusi Olawale <olawalequest@gmail.com>
2016-10-18 11:36:36 +0100

    test cases errors corrected

--- tests.js
+++ tests.js
@@ -9,67 +9,67 @@ var lib = require('./lib/sumofprimes.js');
 describe("test that the sum of all prime numbers in n are computed correctly", function(){
   it("should give 5 for when n is 3", function(){
     assert(
-        lib.sumOfPrimes(3),{sum:5});
+        lib.sumOfPrimes(3)===5);
   });
 
   it("should return 2 for when n is 2", function(){
     assert(
-        lib.sumOfPrimes(2),{sum:2});
+        lib.sumOfPrimes(2)===2);
   });
 
   it("should return 17 for when n is 10", function(){
-    assert(
-        lib.sumOfPrimes(10),{sum:17});
+   assert(
+       lib.sumOfPrimes(10) === 17);
   });
 
   it("should return 77 for when n is 20", function(){
     assert(
-        lib.sumOfPrimes(20),{sum:77});
+        lib.sumOfPrimes(20)===77);
   });
 
   it("should return 'No prime number' for when n is 0", function(){
     assert(
-        lib.sumOfPrimes(0),"no prime number");
+        lib.sumOfPrimes(0)==="no prime number");
   });
 
   it("should return 'No prime number' for when n is 1", function(){
     assert(
-        lib.sumOfPrimes(0),"no prime number");
+        lib.sumOfPrimes(1)==="no prime number");
   });
 
   it("should return Invalid input for when n is -10", function(){
     assert(
-        lib.sumOfPrimes(-10),"invalid input");
+        lib.sumOfPrimes(-10)==="invalid input");
   });
 
   it("should return Invalid input for when n is wale", function(){
     assert(
-        lib.sumOfPrimes("wale"),"Invalid input");
+        lib.sumOfPrimes("wale")==="invalid input");
   });
 
   it("should return Invalid input for when n is andela", function(){
     assert(
-        lib.sumOfPrimes("andela"),"invalid input");
+        lib.sumOfPrimes("andela")==="invalid input");
   });
 
   it("should return Invalid input for when n is ...", function(){
     assert(
-        lib.sumOfPrimes("..."),"invalid input");
+        lib.sumOfPrimes("...")==="invalid input");
   });
 
   it("should return Invalid input for when n is -40", function(){
     assert(
-        lib.sumOfPrimes(-40),"invalid input");
+        lib.sumOfPrimes(-40)==="invalid input");
   });
 
   it("should return 1060 for when n is 100", function(){
     assert(
-        lib.sumOfPrimes(100),{sum:1060});
+        lib.sumOfPrimes(100)===1060);
   });
 
   it("should return 593823 for when n is 3000", function(){
     assert(
-        lib.sumOfPrimes(100),{sum:593823});
+        lib.sumOfPrimes(3000)===593823);
   });
 
 });
